6a4f64a25d6cb40823d5ce0ddd927ad1a9fc4a44
rayhoward <raymond.a.howard@comcast.net>
2021-07-04 01:22:31 -0400

    Add REST API for controls

--- sketch_jul03a/sketch_jul03a.ino
+++ sketch_jul03a/sketch_jul03a.ino
@@ -5,60 +5,90 @@
  * 
  * ON Board LED GPIO 2
  */
+#include <WiFi.h>
+#include <WebServer.h>
+#include <Arduino.h>
+#include "wifiThings.h"
+
 #define LED 2
-#define GO_PIN 13
-#define YEILD_PIN 14
-#define STOP_PIN 15
+#define GO_PIN 14
+#define YEILD_PIN 15
+#define STOP_PIN 13
 
+// Web server running on port 80
+WebServer server(80);
+void connectToWiFi() {
+  Serial.print("Connecting to ");
+  Serial.println(SSID);
+  
+  WiFi.begin(SSID, PASS);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+    // we can even make the ESP32 to sleep
+  }
+ 
+  Serial.print("Connected. IP: ");
+  Serial.println(WiFi.localIP());
+}
 
+// setup API resources
+void setup_routing() {
+  server.on("/stopOn", StopPinOn);
+  server.on("/stopOff", StopPinOff);
+  server.on("/yeildOn", YeildPinOn);
+  server.on("/yeildOff", YeildPinOff);
+  server.on("/goOn", GoPinOn);
+  server.on("/goOff", GoPinOff);
+ 
+  // start server
+  server.begin();
+}
 
 void setup() {
   // Set pin mode
+  Serial.begin(115200);
   pinMode(LED,OUTPUT);
   pinMode(GO_PIN,OUTPUT);
   pinMode(YEILD_PIN,OUTPUT);
   pinMode(STOP_PIN,OUTPUT);
   
   randomSeed(analogRead(0));
+  connectToWiFi();
+  setup_routing();
 }
 
 void loop() {
- // delay(500);
-  //digitalWrite(LED,HIGH);
-  
-  GoPinOn();
-  delay(random(10000,30000));
-  GoPinOff();
-  YeildPinOn();
-  delay(random(10000,30000));
-  YeildPinOff();
-  StopPinOn();
-  delay(random(15000,45000));
-  StopPinOff();
- // delay(500);
- // digitalWrite(LED,LOW);
+  server.handleClient();
 }
 
 void GoPinOn() {
   digitalWrite(GO_PIN,HIGH);
+  server.send(200);
 }
 
 void GoPinOff() {
   digitalWrite(GO_PIN,LOW);
+  server.send(200);
 }
 
 void YeildPinOn() {
   digitalWrite(YEILD_PIN,HIGH);
+  server.send(200);
 }
 
 void YeildPinOff() {
   digitalWrite(YEILD_PIN,LOW);
+  server.send(200);
 }
 
 void StopPinOn() {
   digitalWrite(STOP_PIN,HIGH);
+  server.send(200);
 }
 
 void StopPinOff() {
   digitalWrite(STOP_PIN ,LOW);
+  server.send(200);
 }
